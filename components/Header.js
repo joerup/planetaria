0685f7b3dfef3ddcc71694b0461cdab1a6bad119
Joe Rupertus <joerup2004@gmail.com>
2021-02-15 18:07:06 -0500

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ export default function Header({ props }) {
 
         <div className={styles.search}>
           <div className={styles.searchBar}>
-            <NextLink href="../search">
+            <NextLink href="https://planetaria.app/search">
               <Link>
                 <p className={styles.searchTitle}>Search</p>
               </Link>
@@ -31,20 +31,20 @@ export default function Header({ props }) {
         <div className={styles.dropdown}>
           <img className={styles.dropbutton} src="https://img.icons8.com/metro/26/ffffff/menu.png"/>
           <div className={styles.dropdowncontent}>
-            <a href="../solarsystem">System</a>
-            <a href="../galaxy">Galaxy</a>
-            <a href="../about">About</a>
+            <a href="https://planetaria.app/solarsystem">System</a>
+            <a href="https://planetaria.app/galaxy">Galaxy</a>
+            <a href="https://planetaria.app/about">About</a>
             <a href="https://apps.apple.com/is/app/planetaria/id1546887479">App</a>
           </div>
         </div>
 
-        <NextLink href="../solarsystem">
+        <NextLink href="https://planetaria.app/solarsystem">
           <Link><p className={styles.rowItem}>System</p></Link>
         </NextLink>
-        <NextLink href="../galaxy">
+        <NextLink href="https://planetaria.app/galaxy">
           <Link><p className={styles.rowItem}>Galaxy</p></Link>
         </NextLink>
-        <NextLink href="../about">
+        <NextLink href="https://planetaria.app/about">
           <Link><p className={styles.rowItem}>About</p></Link>
         </NextLink>
         <a href="https://apps.apple.com/is/app/planetaria/id1546887479"><p className={styles.rowItem}>App</p></a>
